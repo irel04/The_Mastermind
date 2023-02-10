7350f886a6bba120aedb86fb7f415b2d8ea0fd22
Mark Lhester Agellon <kianirel56@gmail.com>
2023-02-10 17:50:05 +0800

    Create functions for generating the color combinations and getting input from the user

--- main.py
+++ main.py
@@ -5,4 +5,18 @@ COLORS = ["R", "G", "B", "Y", "W", "O"]
 TRIES = 10
 CODE_LENGTH = 4
 
-# Creating our own function to run for the game
+# Creating our function for creating 4-color combination from the COLORS list
+def generate_code():
+    code = []
+
+    for i in range(CODE_LENGTH):
+        color = random.choice(COLORS)
+        code.append(color)
+
+    return code
+
+# This function will allow us to get input from the users
+def player_guess():
+    guess = input("Guess the color combination: ").upper().split(" ") # Split method allow us to separate the input and turn it into a list
+    
+
