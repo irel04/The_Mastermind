5fac361af591930bd7562b44e4020b7e1c876cee
Mark Lhester Agellon <kianirel56@gmail.com>
2023-02-11 17:57:49 +0800

    Creating a custom tkinter

--- GUI.py
+++ GUI.py
@@ -1,9 +1,18 @@
 import customtkinter
-
+from modified_project import *
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry('500x350')
+root.geometry('700x350')
 
-frame = customtkinter.CTkFrame(maste=root)
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill='both', expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Welcome to Mastermind", font=('Roboto', 20))
+label.pack(pady=12, padx=10)
+
+label_2 = customtkinter.CTkLabel(master=frame, text=f"You have {TRIES} tries to guess the color combinations", font=('Roboto', 15))
+label_2.pack(pady=13, padx=10)
+
+root.mainloop()
