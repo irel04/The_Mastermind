7d91ba97ef280fb1b53fc58a2ad89250c3f1a71d
Mark Lhester Agellon <kianirel56@gmail.com>
2023-02-13 17:08:14 +0800

    GUI added label for color available

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import customtkinter
 from modified_project import *
+import tkinter
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
@@ -16,13 +17,20 @@ class App(customtkinter.CTk):
         super().__init__(*args, **kwargs)
         self.geometry("500x400")
 
-        self.game_title = customtkinter.CTkLabel(self, text="Welcome to MasterMind",width=280, height= 70, 
-        fg_color=("white", "gray75"), corner_radius=8, font=())
+        self.game_title = customtkinter.CTkLabel(self, text="Welcome to MasterMind",width=280, height= 40, 
+        fg_color=("#4B7CE7", "#1E5CE1"), corner_radius=8, font=("Roboto", 23))
         self.game_title.pack(side="top", padx=20, pady=20)
 
-        self.button_1 = customtkinter.CTkButton(self, text="open toplevel", command=self.open_toplevel)
-        self.button_1.pack(side="top", padx=20, pady=20)
+        self.available_colors = customtkinter.CTkLabel(self, text="Available colors",width=100, height= 30, 
+        fg_color=("#4B7CE7", "#1E5CE1"), corner_radius=8, font=("Roboto", 15))
+        self.available_colors.pack(side="top", padx=20, pady=20)
 
+        self.color_list = customtkinter.CTkLabel(self, text=COLORS, font=('Roboto', 15))
+        self.color_list.place(relx=0.415, rely=0.35)
+
+        self.button_1 = customtkinter.CTkButton(self, text="Submit", command=self.open_toplevel)
+        self.button_1
+        
         self.toplevel_window = None
 
     def open_toplevel(self):
@@ -34,3 +42,4 @@ class App(customtkinter.CTk):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+
